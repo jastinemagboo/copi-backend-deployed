21d416f07162440d6efa8d9b3d31a2a8d33d38fd
jastine <jastinemagboo25@gmail.com>
2025-05-15 18:25:00 +0800

    Fix the UTC timezone

--- models/post_model.py
+++ models/post_model.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from extensions import db  # ✅ not from app
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
@@ -9,7 +9,7 @@ class Post(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
     updated_at = db.Column(db.DateTime, nullable=True)
     deleted_at = db.Column(db.DateTime, nullable=True)
 
